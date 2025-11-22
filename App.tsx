0308b60765de072d74240873b99e44f4fa1b9b9a
František <f_lamacek@utb.cz>
2025-11-22 13:09:54 +0100

    Navigation - app navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>This is created by Frantisek Lamacek</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppNavigator />
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#355',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
