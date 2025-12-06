16eee1fcad9a1dd70f3772b3c0aa0849519ebcf5
František <f_lamacek@utb.cz>
2025-12-06 10:11:14 +0100

    Testing API data via console.log

--- App.tsx
+++ App.tsx
@@ -1,6 +1,15 @@
 import AppNavigator from './navigation/AppNavigator';
+import { useEffect } from 'react';
+import { getStockQuote } from './src/api/stocksApi';
 
 export default function App() {
+  useEffect(() => {
+    getStockQuote("AAPL")
+      .then((data) => console.log("AAPL data:", data))
+      .catch((err) => console.log("API ERROR:", err));
+  }, []);
+
+
   return <AppNavigator />
 }
 
