9f2188409bc82acdbc222747e67b1f6316ee7aa2
František <f_lamacek@utb.cz>
2025-12-06 19:18:59 +0100

    Changing libraries from victory-native to svg (looks bad)

--- src/api/stocksApi.ts
+++ src/api/stocksApi.ts
@@ -1,13 +1,14 @@
 const API_KEY = process.env.EXPO_PUBLIC_ALPHA_KEY;
 const BASE_URL = "https://www.alphavantage.co/query";
 
+// ----- 1) aktualni cena akcie -----
 export async function getStockQuote(symbol: string) {
     const url = `${BASE_URL}?function=GLOBAL_QUOTE&symbol=${symbol}&apikey=${API_KEY}`;
 
     const response = await fetch(url);
 
     if (!response.ok) {
-        throw new Error("Network errori při volání API");
+        throw new Error("Network error při volání API");
     }
 
     const data = await response.json();
@@ -17,8 +18,30 @@ export async function getStockQuote(symbol: string) {
     }
 
     return {
-        symbol,
         price: parseFloat(data["Global Quote"]["05. price"]),
         change: parseFloat(data["Global Quote"]["10. change percent"].replace("%", "")),
     };
+}
+
+// -----2) historicka cast akcie -----
+export async function getStockHistory(symbol: string) {
+    const url = `${BASE_URL}?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${API_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const series = data["Time Series (Daily)"];
+    if (!series) {
+        throw new Error("Chyba načtení historických dat");
+    }
+
+    const result = Object.keys(series)
+        .slice(0, 10)
+        .map((date) => ({
+        date,
+        price: parseFloat(series[date]["4. close"]),
+        }))
+        .reverse();
+
+    return result;
 }
--- src/screens/StockDetailScreen.tsx
+++ src/screens/StockDetailScreen.tsx
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/AppNavigator";
 import { getStockQuote } from "../api/stocksApi";
+import { getStockHistory } from "../api/stocksApi";
+import Svg, { Polyline } from "react-native-svg";
 
 type StockDetailNavigationProp = NativeStackNavigationProp<
         RootStackParamList,
@@ -20,12 +22,18 @@ export default function StockDetailScreen({ navigation, route }: Props) {
     const [price, setPrice] = useState<number | null>(null);
     const [change, setChange] = useState<number | null>(null);
     const [loading, setLoading] = useState(true);
+    const [history, setHistory] = useState<{ date: string; price: number }[]>([]);
 
     useEffect(() => {
         async function load() {
         const data = await getStockQuote(symbol);
         setPrice(data.price);
         setChange(data.change);
+
+        const hist = await getStockHistory(symbol);
+        setHistory(hist);
+        console.log("HISTORY:", hist);
+
         setLoading(false);
         }
         load();
@@ -40,10 +48,27 @@ export default function StockDetailScreen({ navigation, route }: Props) {
         );
     }
 
+// ----- Normalizace pro GRAF SVG... -----
+    const maxPrice = Math.max(...history.map((h) => h.price));
+    const minPrice = Math.min(...history.map((h) => h.price));
+    const range = maxPrice - minPrice || 1;
+
+    const chartWidth = 350;
+    const chartHeight = 200;
+
+    const points = history
+        .map((p, i) => {
+        const x = i * (chartWidth / (history.length - 1));
+        const y = chartHeight - ((p.price - minPrice) / range) * chartHeight;
+        return `${x},${y}`;
+        })
+        .join(" ");
+
     return (
         <View style={styles.container}>
         <Text style={styles.symbol}>{symbol}</Text>
         <Text style={styles.price}>{price?.toFixed(2)} $</Text>
+
         <Text
             style={[
             styles.change,
@@ -53,6 +78,18 @@ export default function StockDetailScreen({ navigation, route }: Props) {
             {change! >= 0 ? "+" : ""}
             {change?.toFixed(2)} %
         </Text>
+
+        {/* GRAF */}
+        <View style={{ marginTop: 40 }}>
+            <Svg height={chartHeight} width={chartWidth}>
+            <Polyline
+                points={points}
+                fill="none"
+                stroke="#3b82f6"
+                strokeWidth="3"
+            />
+            </Svg>
+        </View>
         </View>
     );
 }
